49f062a60bb4b91ffcf430979b85a0588b7d104b
askyourself <askyourself.co@gmail.com>
2015-03-26 08:46:17 -0700

    Update grayscale.js

--- js/grayscale.js
+++ js/grayscale.js
@@ -38,7 +38,7 @@ function init() {
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     var mapOptions = {
         // How zoomed in you want the map to start at (always required)
-        zoom: 10,
+        zoom: 5,
 
         // The latitude and longitude to center the map (always required)
         center: new google.maps.LatLng(37.235,-115.811111),
